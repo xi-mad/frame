e93055b78d26226b2d143e76fa38ddbd63bdc743
liuxudong <1171866049@qq.com>
2021-01-12 19:05:52 +0800

    add resources

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/image')
+def image():
+    return render_template('image.html')
+
+
+@app.route('/video')
+def video():
+    return render_template('video.html')
 
 
 if __name__ == '__main__':
